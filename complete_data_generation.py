89653e31e1bef7a3926394f0239de72428ca9b08
Panagiotis Terzopoulos <panos@deepcure.ai>
2021-01-02 12:41:22 -0500

    renew data

---
+++ complete_data_generation.py
@@ -0,0 +1,23 @@
+import dill
+from tqdm import tqdm
+
+from main.data_generation import profile_generation
+
+num_voters = [10, 20, 50]
+num_alternatives = [3, 4, 5]
+methods = ['ic', '2urn']
+
+comb = []
+for v in num_voters:
+    for alt in num_alternatives:
+        for m in methods:
+            comb.append((v, alt, m))
+
+profiles = {}
+for c in tqdm(comb):
+    print(c)
+    profiles[c] = {
+        i: profile_generation(c[1], c[0], c[2], complete=True) for i in tqdm(range(200), desc='random iterations')
+    }
+    with open('data/our_data_complete.pkl', 'wb') as f:
+        dill.dump(profiles, f)
--- main/data_generation.py
+++ main/data_generation.py
@@ -1,11 +1,11 @@
 import math
 import os
+import random
 import sys
 from typing import List
 
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 
 curr_path = os.path.realpath(__file__)
 if curr_path.split('/iterative_voting/main')[0] not in sys.path:
@@ -30,7 +30,7 @@ def fix_symmetry_diagonal(pref: np.array) -> np.array:
     return pref
 
 
-def profile_generation(alt_number: int, vot_number: int, method: str) -> List[pd.DataFrame]:
+def profile_generation(alt_number: int, vot_number: int, method: str, complete: bool) -> List[pd.DataFrame]:
     """
         Inputs:
          alt_number: the number of alternatives
@@ -38,39 +38,56 @@ def profile_generation(alt_number: int, vot_number: int, method: str) -> List[pd
          method: the way that we want the profile to be created. e.g., what kind of distribution to sample from.
           we include "ic" for impartial culture when we create the preference uniformly at random and
           "2urn" for the 2urn model (see literature for details on the definition of this)
+          complete: whether to generate complete or incomplete preferences
         Returns:
         A profile: a list of arrays
     """
+    if complete:
+        func = generate_complete_random_preference
+    else:
+        func = generate_incomplete_random_preference
+
     gprofile = []
 
     assert method in ['ic', '2urn']
 
     if method == 'ic':
         for _ in range(vot_number):
-            some_pref = generate_random_preference(alt_number)
+            some_pref = func(alt_number)
             gprofile.append(some_pref)
     else:
-        pref_1 = generate_random_preference(alt_number)
-        pref_2 = generate_random_preference(alt_number)
+        pref_1 = func(alt_number)
+        pref_2 = func(alt_number)
         while pref_2.equals(pref_1):
-            pref_2 = generate_random_preference(alt_number)
+            pref_2 = func(alt_number)
 
         for _ in range(math.floor(vot_number / 3)):
             gprofile.append(pref_1)
             gprofile.append(pref_2)
 
         for i in range(vot_number - 2 * math.floor(vot_number / 3)):
-            gprofile.append(generate_random_preference(alt_number))
+            gprofile.append(func(alt_number))
 
     # TODO: create 3urn method as well
 
     return gprofile
 
 
-def generate_random_preference(alt_number: int):
+def generate_incomplete_random_preference(alt_number: int) -> pd.DataFrame:
     random_pref = np.random.randint(-1, 2, (alt_number, alt_number))
     some_pref = pd.DataFrame(fix_symmetry_diagonal(random_pref))
     while not check_transitivity(some_pref):
         random_pref = np.random.randint(-1, 2, (alt_number, alt_number))
         some_pref = pd.DataFrame(fix_symmetry_diagonal(random_pref))
     return some_pref
+
+
+def generate_complete_random_preference(alt_number: int) -> pd.DataFrame:
+    order = list(range(alt_number))
+    random.shuffle(order)
+    new_mat = np.ones((alt_number, alt_number))
+    for counter, alt in enumerate(order):
+        for i in range(0, counter):
+            new_mat[alt, order[i]] = -1
+    np.fill_diagonal(new_mat, 0)
+    return pd.DataFrame(new_mat).astype(int)
