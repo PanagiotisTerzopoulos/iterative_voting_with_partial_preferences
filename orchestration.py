03d89348bd4968605b8d73519ab644e1104517bb
Panagiotis Terzopoulos <panos@deepcure.ai>
2021-01-13 05:47:16 -0500

    update plots notebook and break number of iterations if not manipulable profile

--- orchestration.py
+++ orchestration.py
@@ -8,7 +8,6 @@ from main.orchestration import voting_iteration
 
 
 def main(args):
-
     if args.complete_profiles:
         suffix = '_complete'
     else:
@@ -23,8 +22,8 @@ def main(args):
     data_to_use = all_data[(args.num_voters, args.num_alt, args.data_type)]
     print(f'running {args}')
 
-    if os.path.isfile('data/results/total_result_veto_4.pkl'):
-        with open('data/results/total_result_veto_4.pkl', 'rb') as f:
+    if os.path.isfile('data/results/total_result.pkl'):
+        with open('data/results/total_result.pkl', 'rb') as f:
             total_result = dill.load(f)
     else:
         total_result = {}
@@ -33,6 +32,7 @@ def main(args):
 
     for random_profile in tqdm(prof_indices_to_run, desc='random profiles'):
         all_preferences = data_to_use[random_profile]
+        break_the_same_profile_iteration = False
         for meta_counter in range(args.num_iterations):
             key = (
                 args.num_alt, args.num_voters, args.data_type, random_profile, args.k, args.method, args.cycle_limit,
@@ -57,9 +57,16 @@ def main(args):
                 else:
                     convergence_happened, res_dict = result
                     total_result[key] = (convergence_happened, res_dict)
+                    if res_dict.keys().max() == 0:
+                        # if it cannot manipulate for this profile then it doesn't make sense running the profile
+                        # multiple times cause the random order doesn't play a role
+                        break_the_same_profile_iteration = True
 
-                with open('data/results/total_result_veto_4.pkl', 'wb') as f:
+                with open('data/results/total_result.pkl', 'wb') as f:
                     dill.dump(total_result, f)
+
+            if break_the_same_profile_iteration:
+                break
 
 
 if __name__ == '__main__':
